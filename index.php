dc34b7c1e6faacde2719a88fe02fed012a0e6ade
Toshiaki Maki <tmaki@pivotal.io>
2018-04-19 03:35:32 +0900

    V5

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <?php
-echo 'V4 ' . $_ENV['CF_INSTANCE_IP'] . ' / ' . $_ENV['CF_INSTANCE_INDEX'] . ' / ' . $_ENV['CF_INSTANCE_INTERNAL_IP'] . "\n";
+echo 'V5 ' . $_ENV['CF_INSTANCE_IP'] . ' / ' . $_ENV['CF_INSTANCE_INDEX'] . ' / ' . $_ENV['CF_INSTANCE_INTERNAL_IP'] . "\n";
